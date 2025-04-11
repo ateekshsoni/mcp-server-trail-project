114e3080ca66c6ed53010e741a75009276573133
user.email : "ateekshsoni@gmail.com <user.email "ateekshsoni@gmail.com>
2025-04-11 18:59:40 +0530

    refactor: clean up formatting and improve structure in mcp.tool.js

--- server/mcp.tool.js
+++ server/mcp.tool.js
@@ -1,23 +1,27 @@
+//mcp tool file for create post
 import { config } from "dotenv";
-import { text } from "express";
 import { TwitterApi } from "twitter-api-v2";
 config();
 const twitterClient = new TwitterApi({
-    appKey: process.env.TWITTER_API_KEY,
-    appSecret: process.env.TWITTER_API_KEY_SECRET,
-    accessToken: process.env.TWITTER_ACCESS_TOKEN,
-    accessSecret: process.env.TWITTER_ACCESS_SECRET,
-})
+  appKey: process.env.TWITTER_API_KEY,
+  appSecret: process.env.TWITTER_API_KEY_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_SECRET,
+});
 
-export async function createPost(status){
-    const newPost = await twitterClient.v2.tweet(status)
+export async function createPost(status) {
+  try {
+    const newPost = await twitterClient.v2.tweet(status);
 
     return {
-        content : [
-            {
-                type : text,
-                text : `Tweet created with ID: ${newPost.data.id} and text: ${newPost.data.text} with status : ${status}`
-            }
-        ]
-    }
+      content: [
+        {
+          type: text,
+          text: `Tweet created with ID: ${newPost.data.id} and text: ${newPost.data.text} with status : ${status}`,
+        },
+      ],
+    };
+  } catch (err) {
+    console.log(err);
+  }
 }
